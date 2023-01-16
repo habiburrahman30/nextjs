e04861b43d2ee2839dccf28e8deea5897b20df57
habiburrahman30 <hmhabib999@gmail.com>
2023-01-16 18:03:53 +0600

    done

--- app/sidebar/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default () => {
-    return (
-        <aside className="flex justify-center bg-indigo-500 rounded-lg py-4">
-            <Link href={'/'} className="text-2xl font-bold text-gray-50">Navigate</Link>
-        </aside>
-    )
-}
